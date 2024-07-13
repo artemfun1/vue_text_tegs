a5d346a2c5fabf575f3b225178576454c54716cf
artemfun1 <a.o.peshkov@yandex.ru>
2024-07-13 18:26:32 +0500

    hard

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,46 @@ import TextTeg from "./components/TextTeg.vue";
 
 const arr = ref([
 	{
-		text: "текст1",
+		text: "текст1 текст1 текст1",
 	},
 	{
-		text: "текст2",
-		icon: "название иконки",
+		text: "текст2 текст2 текст2  текст2 текст2 ",
+		icon: "mdi-home",
 	},
-  {
-		text: "текст3",
-		icon: "название иконки",
+	{
+		text: "текст3 текст3 текст3",
+		icon: "mdi-account",
 	},
-  {
-		text: "текст4",
-		
-	},
+	// {
+	// 	text: "текст4 текст4 текст4",
+	// },
 ]);
 
-const number = ref(4)
 
+
+
+const position = ref(false);
+
+function changePosition() {
+	console.log(position.value);
+	position.value = !position.value;
+}
 </script>
 
 <template>
-	<TextTeg :arr='arr' />
+	<TextTeg :arr="arr" :toggleClass="position" />
+
+	<div class="btn">
+		<v-btn 
+		variant="tonal" @click="changePosition">Изменить</v-btn></div>
 </template>
 
 <style lang="scss">
-body{
-  width: 100%;
-  height: 100%;
+body {
+	width: 100%;
+	height: 100%;
+}
+.btn {
+	margin-top: 10px;
 }
 </style>
--- src/components/TextTeg.vue
+++ src/components/TextTeg.vue
@@ -1,40 +1,91 @@
 <script setup>
-import { ref } from "vue";
+import { computed } from "vue";
 
-defineProps({
+const props = defineProps({
 	arr: Array,
+	toggleClass: Boolean,
 });
 
-const count = ref(0);
+const data = [];
+props.arr.forEach(e => {
+	data.push({
+		text: e.text.trim().split(" "),
+		icon: e.icon ? e.icon : 0,
+	});
+});
+
+data.forEach(e=>{
+	e.text= e.text.filter(e=>e!=="")
+})
+
+
+console.log(data);
+
+const finalData = structuredClone(data);
+
+finalData.forEach(e => {
+		if(e.icon ===0){
+			const array = [];
+			for (let i = 0; i < e.text.length ; i++){
+				array.push(`<p>${e.text[i]} </p>`);
+			}
+			e.text = array;
+		}
+
+
+	if (e.icon !== 0) {
+		const num = e.text.length - 1;
+
+		const array = [];
+		let isIcon = true;
+		let count = 0;
+		for (let i = 0; i < e.text.length + num; i++) {
+			if (isIcon) {
+				array.push(`<p>${e.text[i - count]} </p>`);
+				isIcon = !isIcon;
+				count += 1;
+			} else if (!isIcon) {
+				array.push(`<v-icon :icon='${e.icon}'/>`);
+				isIcon = !isIcon;
+			}
+		}
+		if (array.length === 2) {
+			e.text = array;
+		} else {
+			if (array[array.length - 1] === e.icon) {
+				array.pop();
+			}
+			e.text = array;
+		}
+	}
+});
+
+console.log(finalData);
 </script>
 
 <template>
-  <v-icon icon="mdi-home" />
+	<div class="text-tags_item" v-for="item in arr" :key="item.text">
+		<p class="left" :class="{ active: toggleClass }">{{ item.text }}</p>
 
-  
-		<v-list lines="one">
-			<v-list-item
-				v-for="item in arr"
-				:key="item.text"
-				:title="item.icon"
-        :subtitle="item.text"
-			>
-      <!-- <v-icon icon="mdi-account" /> -->
-    </v-list-item>
-		</v-list>
-
+		<div class="v-icon"><v-icon :icon="item.icon" /></div>
+	</div>
 </template>
 
 <style scoped lang="scss">
-.text-tags {
-	width: 100dvw;
-	height: 100dvh;
-	background-color: #5ad0d2;
-
-	.text-tags_item {
+.text-tags_item {
+	.left {
+		display: block;
+		width: 100%;
+		text-align: end;
+		background-color: #be7676;
+	}
+	.active {
 		display: flex;
-		gap: 10px;
-		background: #9d9d9d;
+		width: 100%;
+		text-align: justify;
+		background-color: #529a8c;
+	}
+	.v-icon {
 	}
 }
 </style>
