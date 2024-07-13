a763b1f33a7361cbeb88aca3a2ab4615772b2411
artemfun1 <a.o.peshkov@yandex.ru>
2024-07-13 13:45:41 +0500

    add component

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+import TextTeg from "./components/TextTeg.vue";
+
+const arr = ref([
+	{
+		text: "текст1",
+	},
+	{
+		text: "текст2",
+		icon: "название иконки",
+	},
+  {
+		text: "текст3",
+		icon: "название иконки",
+	},
+  {
+		text: "текст4",
+		
+	},
+]);
+
+const number = ref(4)
+
+</script>
+
+<template>
+	<TextTeg :arr='arr' />
+</template>
+
+<style lang="scss">
+body{
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/TextTeg.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+	arr: Array,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <v-icon icon="mdi-home" />
+
+  
+		<v-list lines="one">
+			<v-list-item
+				v-for="item in arr"
+				:key="item.text"
+				:title="item.icon"
+        :subtitle="item.text"
+			>
+      <!-- <v-icon icon="mdi-account" /> -->
+    </v-list-item>
+		</v-list>
+
+</template>
+
+<style scoped lang="scss">
+.text-tags {
+	width: 100dvw;
+	height: 100dvh;
+	background-color: #5ad0d2;
+
+	.text-tags_item {
+		display: flex;
+		gap: 10px;
+		background: #9d9d9d;
+	}
+}
+</style>
